043e670fbc54210acd4b0f323f95841e0cc495c6
Amós <amos.silva@gmail.com>
2019-11-24 13:53:07 -0300

    feat(validação): cria factory para a validação
    Para comnseguir mockar o validationTester, preciso que os
    validadores sejam fornecidos por fora. Foi usada a estratégia
    Factory, para instanciar o validador.

---
+++ src/validation/validationTesterFactory.js
@@ -0,0 +1,8 @@
+import brand from './brand'
+import validationTest from './validationTester'
+
+export default () =>{
+  return new validationTest([
+    brand
+  ])
+}
--- test/validation/validationTester.test.js
+++ test/validation/validationTester.test.js
@@ -1,9 +1,18 @@
 import ValidationTester from '../../src/validation/validationTester'
+import ValidatorFactory from '../../src/validation/validationTesterFactory'
 
 describe('card validation tester', () => {
 
+  it('should create a instantied validator tester', ()=>{
+
+    const validationTester = ValidatorFactory()
+
+    expect(validationTester).toBeInstanceOf(ValidationTester)
+  })
+
   it('should instantiate, receive payment data and return true if validation are true', ()=>{
-    const validationTester = new ValidationTester()
+
+    const validationTester = ValidatorFactory()
     const card = {}
 
     expect(validationTester.isCardValid(card)).toBe(true)
