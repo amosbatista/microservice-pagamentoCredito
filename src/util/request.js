5fd85f831147cd1753dc21fb31ef5da0cca47891
Amós <amos.silva@gmail.com>
2019-11-25 00:33:25 -0300

    feat(util): refatora teste de http, usando mock
    Consegui usar mock. Mas tive que mudar o objeto de requisição HTTP para axios, que se comportou melhor para testes.
    
    Consegui mockar a resposta, usando o automock. Avisar que é o jeito mais fácil de usar mock.
    Falta implementar o teste completo, e testar a API para ver se ela continua funcionando.

--- src/util/request.js
+++ src/util/request.js
@@ -1,10 +1,21 @@
+import httpReq from 'axios'
+
 export default {
-  post: async (request)=>{
-    return await {
-      status: 0,
-      body: {
-        test: "test"
-      }
-    }
+  post: (request)=>{
+
+    return new Promise((resolve, reject)=>{
+      
+      httpReq({
+        method: 'post',
+        url: request.url,
+        data: request.body,
+        headers: request.headers
+      }).then((result)=>{
+        resolve(result.data)
+      }).catch((err)=>{
+        reject(err)
+      })
+      
+    })
   }
 }
