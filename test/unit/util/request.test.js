a44df8186ef2dcf9df31829503668475c219f67c
Amós <amos.silva@gmail.com>
2019-11-25 00:33:25 -0300

    feat(util): refatora teste de http, usando mock
    Consegui usar mock. Mas tive que mudar o objeto de requisição HTTP para axios, que se comportou melhor para testes.
    
    Consegui mockar a resposta, usando o automock. Avisar que é o jeito mais fácil de usar mock.
    Falta implementar o teste completo, e testar a API para ver se ela continua funcionando.

--- test/unit/util/request.test.js
+++ test/unit/util/request.test.js
@@ -1,18 +1,30 @@
-import request from '../../../src/util/request' 
+jest.mock('axios')
+import library from 'axios'
+
+import service from '../../../src/util/request'
 
 describe('http request', ()=>{
   it('should receive a post request data and return a post response', async ()=>{
     const data = {
+      url: '/ping',
       body: {
         foo: "bar"
       },
-      header: [
-        {foo: "bar"}
-      ]
+      header: {foo: "bar"}
     }
-    const result = await request.post(data)
+    const result = {
+      data: {
+        status: 0,
+        body: {
+          test: "test"
+        }
+      }
+    }
+    library.mockResolvedValue(result)
+    
+    const reqResult = await service.post(data)
 
-    expect(result).toEqual({
+    expect(reqResult).toEqual({
       status: 0,
       body: {
         test: "test"
