3199db5b4f9c7bcb1c76319541560d8b8247717b
amos Batista <amos.silva@gmail.com>
2020-01-22 10:13:13 -0300

    fix(test): remove index.test
    This code was unecessary to the project, since it was a test to try show the test cover of all source code.

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-import index from '../src/index'
-
-describe('project',()=>{
-  const moduleType = typeof index
-  it('should be a function', ()=>{
-    expect(moduleType).toBe("function");
-  })
-})
